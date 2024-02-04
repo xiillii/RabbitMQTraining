6d0c88b98a55042a8a04f64369a4613f02c84ab1
Jose Alonso <mail@josealonso.dev>
2024-02-03 20:11:55 -0600

    Basic Producer/Consumer

---
+++ ProducerConsumerRabbitMQ/Consumer/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost"};
+
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    var consumer = new EventingBasicConsumer(channel);
+
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body;
+        var message = Encoding.UTF8.GetString(body.ToArray());
+
+        Console.WriteLine("Received message {0}", message);
+    };
+
+    channel.BasicConsume("BasicTest", true, consumer);
+
+}
+
---
+++ ProducerConsumerRabbitMQ/Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+
+using (var connection = factory.CreateConnection()) 
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare("BasicTest", false, false, false, null);
+
+    var message = "Getting started with NET Core RabbitMQ";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "BasicTest", null, body);
+
+    Console.WriteLine("Send message {0}...", message);
+}
+
+

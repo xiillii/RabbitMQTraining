e12a97044096085a1f0f3ab9c62fe133ba550655
Jose Alonso <mail@josealonso.dev>
2024-02-03 22:15:53 -0600

    Infra IoC. Implement Register Services
    Add transient  of IEventBus to RabbitMQBus

---
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroRabbit.Infra.IoC;
+
+public class DependencyContainer
+{
+    public static void RegisterServices(IServiceCollection services)
+    {
+        // Domain bus
+        services.AddTransient<IEventBus, RabbitMQBus>();
+    }
+}

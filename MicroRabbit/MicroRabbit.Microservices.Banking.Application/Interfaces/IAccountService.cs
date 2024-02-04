512862136dad473dc7ff10e50029bc5891b3706c
Jose Alonso <mail@josealonso.dev>
2024-02-03 23:13:23 -0600

    Banking Microservice. Implement Application Layer
    Account Service

---
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Application/Interfaces/IAccountService.cs
@@ -0,0 +1,8 @@
+﻿using MicroRabbit.Microservices.Banking.Domain.Models;
+
+namespace MicroRabbit.Microservices.Banking.Application.Interfaces;
+
+public interface IAccountService
+{
+    IEnumerable<Account> GetAccounts();
+}
---
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Application/Services/AccuntService.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Microservices.Banking.Application.Interfaces;
+using MicroRabbit.Microservices.Banking.Domain.Interfaces;
+using MicroRabbit.Microservices.Banking.Domain.Models;
+
+namespace MicroRabbit.Microservices.Banking.Application.Services;
+
+public class AccuntService : IAccountService
+{
+    private readonly IAccountRepository _accountRepository;
+
+    public AccuntService(IAccountRepository accountRepository)
+    {
+        _accountRepository = accountRepository;
+    }
+
+    public IEnumerable<Account> GetAccounts()
+    {
+        return _accountRepository.GetAccounts();
+    }
+}

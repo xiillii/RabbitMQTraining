21dc676bb96f67d4e9197e8331f3d4d68716d6be
Jose Alonso <mail@josealonso.dev>
2024-02-04 01:17:43 -0600

    Configure Swagger and MediatR in API Microservice

--- MicroRabbit/MicroRabbit.Microservices.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Api/Program.cs
@@ -2,6 +2,7 @@ using MicroRabbit.Infra.IoC;
 using MicroRabbit.Microservices.Banking.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +11,19 @@ builder.Services.AddDbContext<BankingDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
 });
+var assembly = Assembly.GetExecutingAssembly();
+builder.Services.AddMediatR(conf => conf.RegisterServicesFromAssemblies(assembly));
 DependencyContainer.RegisterServices(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc(
+        "v1",
+        new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
+});
 
 var app = builder.Build();
 

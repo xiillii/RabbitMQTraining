43d0747d24b3b8ff008f31ebf359f5e406f7f93b
Jose Alonso <mail@josealonso.dev>
2024-02-03 22:53:58 -0600

    Banking Microservice. Implement Account Repository

---
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,20 @@
+﻿using MicroRabbit.Microservices.Banking.Data.Context;
+using MicroRabbit.Microservices.Banking.Domain.Interfaces;
+using MicroRabbit.Microservices.Banking.Domain.Models;
+
+namespace MicroRabbit.Microservices.Banking.Data.Repository;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly BankingDbContext _ctx;
+
+    public AccountRepository(BankingDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public IEnumerable<Account> GetAccounts()
+    {
+        return _ctx.Accounts;
+    }
+}

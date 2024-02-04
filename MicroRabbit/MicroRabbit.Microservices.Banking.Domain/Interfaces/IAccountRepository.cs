16155836209633b99f9604563966720a9925f88d
Jose Alonso <mail@josealonso.dev>
2024-02-03 22:46:15 -0600

    Banking Microservice. Implement Account Repository Interface

---
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Domain/Interfaces/IAccountRepository.cs
@@ -0,0 +1,8 @@
+﻿using MicroRabbit.Microservices.Banking.Domain.Models;
+
+namespace MicroRabbit.Microservices.Banking.Domain.Interfaces;
+
+public interface IAccountRepository
+{
+    IEnumerable<Account> GetAccounts();
+}

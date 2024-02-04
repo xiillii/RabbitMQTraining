d5f69844221d734ee73c2c15de61e907e93145d0
Jose Alonso <mail@josealonso.dev>
2024-02-03 22:35:16 -0600

    Banking Microservice. Implement Model

---
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Domain/Models/Account.cs
@@ -0,0 +1,8 @@
+﻿namespace MicroRabbit.Microservices.Banking.Domain.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string AccountType { get; set; }
+    public decimal AccountBalance { get; set; }
+}

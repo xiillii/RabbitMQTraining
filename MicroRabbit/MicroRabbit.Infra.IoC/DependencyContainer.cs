1e0ff7a7e7cf56b72a4596b97f49786aa40bfb73
Jose Alonso <mail@josealonso.dev>
2024-02-04 01:02:02 -0600

    Fix Infra IoC DependencyContainer
    Fix error with IMediator

--- MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
+++ MicroRabbit/MicroRabbit.Infra.IoC/DependencyContainer.cs
@@ -1,11 +1,14 @@
-﻿using MicroRabbit.Domain.Core.Bus;
+﻿using MediatR;
+using MicroRabbit.Domain.Core.Bus;
 using MicroRabbit.Infra.Bus;
 using MicroRabbit.Microservices.Banking.Application.Interfaces;
 using MicroRabbit.Microservices.Banking.Application.Services;
 using MicroRabbit.Microservices.Banking.Data.Context;
 using MicroRabbit.Microservices.Banking.Data.Repository;
 using MicroRabbit.Microservices.Banking.Domain.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace MicroRabbit.Infra.IoC;
 
@@ -14,7 +17,11 @@ public class DependencyContainer
     public static void RegisterServices(IServiceCollection services)
     {
         // Domain bus
-        services.AddTransient<IEventBus, RabbitMQBus>();
+        services.AddTransient<IEventBus, RabbitMQBus>(sp =>
+        {
+            
+            return new RabbitMQBus(sp.GetService<IMediator>());
+        });
 
         // Application Services
         services.AddTransient<IAccountService, AccountService>();

6dc59aeffc3880bd94806572f884e7dbbe7f1fbe
Jose Alonso <mail@josealonso.dev>
2024-02-04 01:05:12 -0600

    Microservices Banking. Configure startup to database migrations

--- MicroRabbit/MicroRabbit.Microservices.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Microservices.Banking.Api/Program.cs
@@ -1,6 +1,16 @@
+using MicroRabbit.Infra.IoC;
+using MicroRabbit.Microservices.Banking.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<BankingDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BankingDbConnection"));
+});
+DependencyContainer.RegisterServices(builder.Services);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
